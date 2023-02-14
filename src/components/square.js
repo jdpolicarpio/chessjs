eec95b540fa979efd35fc1b0b54858e260c7a44c
John David Policarpio <johndavidpolicarpio@gmail.com>
2023-02-13 19:50:56 -0500

    Testing develop branch

--- src/components/square.js
+++ src/components/square.js
@@ -15,6 +15,7 @@ import whiteRook from "../images/wr.png";
 
 function Square(props) {
   let squareContent = mapStringToImage(props.piece);
+  //testing new branch
 
   return (
     <div class={props.class} key={props.i}>
